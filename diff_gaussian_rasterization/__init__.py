991a94c58c3688bc465d827cf3ea4148ca19acd8
枫 聚 <1347947622@qq.com>
2023-12-10 10:58:36 +0800

    只渲染但不反向传播ROI

--- diff_gaussian_rasterization/__init__.py
+++ diff_gaussian_rasterization/__init__.py
@@ -28,6 +28,7 @@ def rasterize_gaussians(
     rotations,
     cov3Ds_precomp,
     raster_settings,
+    backward_mask
 ):
     return _RasterizeGaussians.apply(
         means3D,
@@ -39,6 +40,7 @@ def rasterize_gaussians(
         rotations,
         cov3Ds_precomp,
         raster_settings,
+        backward_mask
     )
 
 class _RasterizeGaussians(torch.autograd.Function):
@@ -54,6 +56,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         rotations,
         cov3Ds_precomp,
         raster_settings,
+        backward_mask
     ):
 
         # Restructure arguments the way that the C++ lib expects them
@@ -94,7 +97,7 @@ class _RasterizeGaussians(torch.autograd.Function):
         # Keep relevant tensors for backward
         ctx.raster_settings = raster_settings
         ctx.num_rendered = num_rendered
-        ctx.save_for_backward(colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer)
+        ctx.save_for_backward(colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer,backward_mask)
         return color, radii
 
     @staticmethod
@@ -103,27 +106,41 @@ class _RasterizeGaussians(torch.autograd.Function):
         # Restore necessary values from context
         num_rendered = ctx.num_rendered
         raster_settings = ctx.raster_settings
-        colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer = ctx.saved_tensors
+        colors_precomp, means3D, scales, rotations, cov3Ds_precomp, radii, sh, geomBuffer, binningBuffer, imgBuffer,backward_mask = ctx.saved_tensors
+
+        #对所有的属性进行mask
+        colors_precomp_remian = torch.empty(0)
+        cov3Ds_precomp_remain = torch.empty(0)
+        if colors_precomp.shape[0]!=0:
+            colors_precomp_remian = colors_precomp[backward_mask]
+        means3D_remian = means3D[backward_mask]
+        scales_remain = scales[backward_mask]
+        rotations_remain = rotations[backward_mask]
+        if cov3Ds_precomp.shape[0]!=0:
+            cov3Ds_precomp_remain = cov3Ds_precomp[backward_mask]
+        radii_remain = radii[backward_mask]
+        sh_remain = sh[backward_mask]
+        num_rendered_remain = means3D.shape[0]
 
         # Restructure args as C++ method expects them
         args = (raster_settings.bg,
-                means3D, 
-                radii, 
-                colors_precomp, 
-                scales, 
-                rotations, 
+                means3D_remian, 
+                radii_remain, 
+                colors_precomp_remian, 
+                scales_remain, 
+                rotations_remain, 
                 raster_settings.scale_modifier, 
-                cov3Ds_precomp, 
+                cov3Ds_precomp_remain, 
                 raster_settings.viewmatrix, 
                 raster_settings.projmatrix, 
                 raster_settings.tanfovx, 
                 raster_settings.tanfovy, 
                 grad_out_color, 
-                sh, 
+                sh_remain, 
                 raster_settings.sh_degree, 
                 raster_settings.campos,
                 geomBuffer,
-                num_rendered,
+                num_rendered_remain,
                 binningBuffer,
                 imgBuffer,
                 raster_settings.debug)
@@ -140,16 +157,38 @@ class _RasterizeGaussians(torch.autograd.Function):
         else:
              grad_means2D, grad_colors_precomp, grad_opacities, grad_means3D, grad_cov3Ds_precomp, grad_sh, grad_scales, grad_rotations = _C.rasterize_gaussians_backward(*args)
 
+        #返回完整的梯度
+        grad_means3D_full = torch.zeros_like(means3D)
+        grad_means2D_full = torch.zeros_like(means3D)
+        grad_sh_full = torch.zeros_like(sh)
+        grad_colors_precomp_full = torch.zeros_like(colors_precomp)
+        grad_opacities_full = torch.zeros((means3D.shape[0],0)).cuda()
+        grad_scales_full = torch.zeros_like(scales)
+        grad_rotations_full = torch.zeros_like(rotations)
+        grad_cov3Ds_precomp_full = torch.zeros_like(cov3Ds_precomp)
+
+        grad_means3D_full[backward_mask==1] = grad_means3D
+        grad_means2D_full[backward_mask==1] = grad_means2D
+        grad_sh_full[backward_mask==1] = grad_sh
+        if colors_precomp.shape[0]!=0:
+            grad_colors_precomp_full[backward_mask==1] = grad_colors_precomp 
+        grad_opacities_full[backward_mask==1] = grad_opacities
+        grad_scales_full[backward_mask==1] = grad_scales
+        grad_rotations_full[backward_mask==1] = grad_rotations
+        if cov3Ds_precomp.shape[0]!=0:
+            grad_cov3Ds_precomp_full[backward_mask==1] = grad_cov3Ds_precomp
+
         grads = (
-            grad_means3D,
-            grad_means2D,
-            grad_sh,
-            grad_colors_precomp,
-            grad_opacities,
-            grad_scales,
-            grad_rotations,
-            grad_cov3Ds_precomp,
+            grad_means3D_full,
+            grad_means2D_full,
+            grad_sh_full,
+            grad_colors_precomp_full,
+            grad_opacities_full,
+            grad_scales_full,
+            grad_rotations_full,
+            grad_cov3Ds_precomp_full,
             None,
+            None
         )
 
         return grads
@@ -184,7 +223,7 @@ class GaussianRasterizer(nn.Module):
             
         return visible
 
-    def forward(self, means3D, means2D, opacities, shs = None, colors_precomp = None, scales = None, rotations = None, cov3D_precomp = None):
+    def forward(self, means3D, means2D, opacities, shs = None, colors_precomp = None, scales = None, rotations = None, cov3D_precomp = None, backward_mask = None):
         
         raster_settings = self.raster_settings
 
@@ -216,6 +255,7 @@ class GaussianRasterizer(nn.Module):
             scales, 
             rotations,
             cov3D_precomp,
-            raster_settings, 
+            raster_settings,
+            backward_mask
         )
 
